771fc575479043deb1c22b041e92a7c41475ecc0
adam <adam.bar91@gmail.com>
2020-05-08 14:04:31 -0300

    site ready for launch

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -10,7 +10,7 @@ class DatabaseSeeder extends Seeder {
      */
     public function run() {
         $this->call(UserSeeder::class);
-        $this->call(PostSeeder::class);
+        //$this->call(PostSeeder::class);
         $this->call(ContentSeeder::class);
     }
 }
