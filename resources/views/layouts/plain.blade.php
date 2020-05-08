42bd7636fb2c2f62ed0f380c618de34e9e9e54a9
adam <adam.bar91@gmail.com>
2020-05-08 14:17:03 -0300

    site ready for launch

--- resources/views/layouts/plain.blade.php
+++ resources/views/layouts/plain.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>@yield('title') | {{ config('app.name') }}</title>
+    <title>@yield('title') | Mahattat</title>
     <!-- Favicon -->
     <link rel="shortcut icon" href="{{asset('favicon.ico')}}">
     <!-- Fonts -->
